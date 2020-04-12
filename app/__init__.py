74e99881b6ddc3b5bc414c9d4fd0efbb7a2ca813
Abhi Pillai <abhi@teacherspayteachers.com>
2020-04-12 15:08:03 -0400

    change to url

--- app/__init__.py
+++ app/__init__.py
@@ -7,10 +7,11 @@ import os
 
 # Define WSGI app object
 app = Flask(__name__)
-uri = os.environ['DATABASE_URI'] or DATABASE_URI
+uri = os.environ['DATABASE_URL'] or DATABASE_URI
 
 engine = create_engine(uri)
 app.config["SQLALCHEMY_DATABASE_URI"] = uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 # create db file using SQLAlchemy
 db.create_all()
